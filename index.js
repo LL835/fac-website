f76a30d834a456e29d47e9f019d327d3ca2c343c
ll835 <loktinlo@gmail.com>
2022-07-20 18:00:12 +0100

    add application projets carousel

--- index.js
+++ index.js
@@ -20,3 +20,65 @@ function changeTabs(tab){
     newTab.classList.add("active-tab-content");
 
 }
+
+// change slides when user clicks the next slide button
+const nextSlideButton = document.querySelector(".next-slide");
+nextSlideButton.addEventListener("click", () => {
+    changeSlides(1);
+})
+
+// change slides when user clicks the previous slide button
+
+const previousSlideButton = document.querySelector(".previous-slide");
+previousSlideButton.addEventListener("click", () => {
+    changeSlides(-1);
+})
+
+function changeSlides(direction){
+    const activeSlide = document.querySelector("[data-active-slide]");
+    const slides = document.querySelectorAll(".slide");
+    let slidesArray = Array.from(slides);
+    let newIndex = slidesArray.indexOf(activeSlide) + direction;
+
+    if (newIndex < 0){
+        newIndex = slidesArray.length - 1;
+    }  
+    if (newIndex > slidesArray.length - 1){
+        newIndex = 0;
+    }
+    slidesArray[newIndex].dataset.activeSlide = true;
+    delete activeSlide.dataset.activeSlide;
+
+    const activeSlideIndicator = document.querySelector("[data-active-indicator]");
+    const slideIndicators = document.querySelectorAll(".slide-indicator");
+    let indicatorArray = Array.from(slideIndicators);
+    indicatorArray[newIndex].dataset.activeIndicator = "true";
+    delete activeSlideIndicator.dataset.activeIndicator;
+}
+
+/* Automatically change slides in the carousel every 5 seconds */
+setInterval(() => {
+    changeSlides(1)
+}, 5000)
+
+const indicators = document.querySelectorAll(".slide-indicator")
+indicators.forEach(indicator => indicator.addEventListener("click", (e) => {
+    changeSlidesViaIndicators(e.target)
+}))
+
+function changeSlidesViaIndicators(indicatorClicked){
+    const activeSlideIndicator = document.querySelector("[data-active-indicator]");
+    const activeSlide = document.querySelector("[data-active-slide]");
+    const slideIndicators = document.querySelectorAll(".slide-indicator");
+    const slides = document.querySelectorAll(".slide");
+    let slidesArray = Array.from(slides);
+    let indicatorArray = Array.from(slideIndicators);
+    const newIndicatorIndex = indicatorArray.indexOf(indicatorClicked); // get the index number of the button that was clicked
+
+    delete activeSlideIndicator.dataset.activeIndicator; // remove the active status from the current slide
+    delete activeSlide.dataset.activeSlide; // remove the active status from the current indicator
+    slidesArray[newIndicatorIndex].dataset.activeSlide = "true"; // apply the active status to the slide corresponding to the new index number
+    indicatorArray[newIndicatorIndex].dataset.activeIndicator = "true"; // apply the active status to the indicator that was clicked
+
+}
+
